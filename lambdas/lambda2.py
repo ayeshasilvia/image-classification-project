98a14a40d044d9e3ce9d8179b03424ac345c9cc2
Ayesha Binte Mosaddeque <amosaddeque@salesforce.com>
2023-02-07 21:08:40 +1100

    Add notebook, lamndas, step function definition and step function screenshots.

---
+++ lambdas/lambda2.py
@@ -0,0 +1,29 @@
+import json
+import sagemaker
+import base64
+from sagemaker.predictor import Predictor
+from sagemaker.serializers import IdentitySerializer
+
+# Fill this in with the name of your deployed model
+ENDPOINT = 'image-classification-2023-02-05-07-56-16-111'
+
+def lambda_handler(event, context):
+
+    # Decode the image data
+    image = base64.b64decode(event['body']['image_data'])
+
+    # Instantiate a Predictor
+    predictor = Predictor(endpoint_name=ENDPOINT, sagemaker_session=sagemaker.Session())
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+
+    # Make a prediction:
+    inferences = predictor.predict(image)
+
+    # We return the data back to the Step Function
+    event["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
